312b63368edc5b06375057a40e2a29de8eba55f1
akshayayadav <akshayy.yadav@gmail.com>
2019-07-25 11:17:35 -0400

    updating to accept num cores for snakemake

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+
+: ${num_cores:=1}
+
+while getopts ":c:" opt; do
+  case $opt in
+    c) num_cores="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+
+
 mkdir /data/pfamscan_results
 mkdir /data/matrix_results
 mkdir /data/final_results
-snakemake -s /usr/local/bin/snakefile
+snakemake -s /usr/local/bin/snakefile -j $num_cores
 
 
